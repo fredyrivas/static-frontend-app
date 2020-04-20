248fa977d71f6694d3dfea8875c94961039f5129
Fredy Rivas <fredyrivas@MacBook-Pro-de-Fredy.local>
2020-04-20 13:37:26 -0500

    update to front

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -3,23 +3,14 @@ import { TweenMax } from "gsap";
 
 
 import Preloader from './modules/Preloader.js'
-import BrowserSupport from './modules/BrowserSupport.js'
+
 
 
 
 class App {
     constructor() {
 
-        this.browserSupport = new BrowserSupport({
-            versions: {
-                ch: 68,
-                sa: 11,
-                fi: 61,
-                op: 55,
-                msie: 10
-            },
-            container: '.browser-support'
-        })
+        
 
         this.preloader = new Preloader()
 
