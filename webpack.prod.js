43844172014f407e97c4e3945c705050c7114657
Fredy Rivas <fredyrivas@MacBook-Pro-de-Fredy.local>
2020-05-07 18:32:34 -0500

    minify css tool

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,16 +1,13 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
-//const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const CssnanoPlugin = require('cssnano-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
-    
-    devtool: 'source-map'
 
-    // plugins: [
-    //     new FaviconsWebpackPlugin({
-    //         logo: './src/assets/favicon/fav.png',
-    //         prefix: 'assets/favicons/'
-    //     })
-    // ]
+    optimization: {
+        minimizer: [
+            new CssnanoPlugin()
+        ]
+    }
 })
