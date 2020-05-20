4cc5a0922b475efaf51ed1deb7331100788e1f84
Fredy Rivas <fredy.rivas@terantbwa.com.mx>
2020-05-20 00:03:19 -0500

    add terser plugin

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,15 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common.js');
 const CssnanoPlugin = require('cssnano-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = merge(common, {
     mode: 'production',
 
     optimization: {
         minimizer: [
-            new CssnanoPlugin()
+            new CssnanoPlugin(),
+            new TerserPlugin()
         ]
     }
 })
